302d36738b40d7096a4b53d89785c08cdffb976c
wojciech.sz@masterborn.com <wojciech.sz@masterborn.com>
2026-04-06 23:05:44 +0200

    feat: add composition root

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaEventStore } from "./auction/infrastructure/PrismaEventStore";
+import { AuctionRepository } from "./auction/infrastructure/AuctionRepository";
+import { ActiveAuctionsProjection } from "./auction/infrastructure/projections/ActiveAuctionsProjection";
+import { CreateAuctionHandler } from "./auction/application/commands/CreateAuction";
+import { PlaceBidHandler } from "./auction/application/commands/PlaceBid";
+import { CancelAuctionHandler } from "./auction/application/commands/CancelAuction";
+
+// Infrastructure
+const prisma = new PrismaClient({});
+const activeAuctionsProjection = new ActiveAuctionsProjection(prisma);
+const eventStore = new PrismaEventStore(prisma, [activeAuctionsProjection]);
+const auctionRepository = new AuctionRepository(eventStore);
+
+// Command Handlers
+export const createAuctionHandler = new CreateAuctionHandler(auctionRepository);
+export const placeBidHandler = new PlaceBidHandler(auctionRepository);
+export const cancelAuctionHandler = new CancelAuctionHandler(auctionRepository);
