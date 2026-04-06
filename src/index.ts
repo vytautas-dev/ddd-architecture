fb96dafcba0352a6054b8bb0e025e8ff5544a3c8
wojciech.sz@masterborn.com <wojciech.sz@masterborn.com>
2026-04-06 23:11:49 +0200

    feat: install prisma adapter pg

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaEventStore } from "./auction/infrastructure/PrismaEventStore";
 import { AuctionRepository } from "./auction/infrastructure/AuctionRepository";
 import { ActiveAuctionsProjection } from "./auction/infrastructure/projections/ActiveAuctionsProjection";
@@ -7,7 +9,8 @@ import { PlaceBidHandler } from "./auction/application/commands/PlaceBid";
 import { CancelAuctionHandler } from "./auction/application/commands/CancelAuction";
 
 // Infrastructure
-const prisma = new PrismaClient({});
+const adapter = new PrismaPg({ connectionString: process.env["DATABASE_URL"] });
+const prisma = new PrismaClient({ adapter });
 const activeAuctionsProjection = new ActiveAuctionsProjection(prisma);
 const eventStore = new PrismaEventStore(prisma, [activeAuctionsProjection]);
 const auctionRepository = new AuctionRepository(eventStore);
